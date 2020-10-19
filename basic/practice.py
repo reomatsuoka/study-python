10c30272d51ef2ad3b68ef7bcd40d56be96c5117
reomatsuoka <reo199637458@gmail.com>
2020-10-19 23:08:39 +0900

    practice

--- basic/practice.py
+++ basic/practice.py
@@ -1,15 +1,5 @@
 #coding:utf-8
 #ヒット＆ブロー
-# import random
-# a = random.randint(0,9)
-# print(a)
-
-# b = int(input("数を入力して下さい>>"))
-# if a == b:
-#   print("当たり")
-# else:
-#   print("はずれ")
-
 import random
 a=[random.randint(0,9),random.randint(0,9),random.randint(0,9),random.randint(0,9)]
 print(str(a[0]),str(a[1]),str(a[2]),str(a[3]))
@@ -17,10 +7,10 @@ while True:
 #4桁の数字か判別する式
   isok = False
   while isok == False:
-    b=input("数を入れてね＞")
-    if len(b) !=4:
+    b = input("数を入れてね＞")
+    if len(b) != 4:
       print("４桁の数字を入れて下さい")
-    else:
+  else:
       kazuok = True
       for i in range(4):
         if ( b[i] < "0") or ( b[i] > "9"):
@@ -28,12 +18,12 @@ while True:
           kazuok = False
           break
       if kazuok:
-        isok=True
+        isok = True
 #ヒットを判定
 hit=0
 for i in range(4):
-  if a[i]==int(b[i]):
-    hit=hit+1
+  if a[i] == int(b[i]):
+    hit = hit+1
 
 #ブローを判定
 blow = 0
