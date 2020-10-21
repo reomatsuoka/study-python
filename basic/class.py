ff755dcdf46d3b7581b735ae1b049d9aaf27e783
reomatsuoka <reo199637458@gmail.com>
2020-10-22 00:46:42 +0900

    class

---
+++ basic/class.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+class Ball:
+    def _init_(self, x, y, dx, dy, color):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.color = color
+    def test(self)
+        print(self.x)
+        print(self.y)
