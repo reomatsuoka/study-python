da15007a7327e83257b481cc33134dbb12629e02
reomatsuoka <reo199637458@gmail.com>
2020-10-24 00:40:30 +0900

    qestion

--- basic/class.py
+++ basic/class.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 import tkinter as tk
+
+
 class Ball:
     def _init_(self, x, y, dx, dy, color):
         self.x = x
@@ -8,40 +10,51 @@ class Ball:
         self.dy = dy
         self.color = color
 
-def move(self,canvas):
-        canvas.create_oval(self.x - 21, self.y - 21,self.x + 21, self.y + 21,fill="white", width=0)
-        self.x = self.x + self.dx
-        self.y = self.y + self.dy 
-        canvas.create_oval(self.x - 20, self.y - 20, self.x +20, self.y + 20, fill=b["color"], width=0)
-        if self.x >= canvas.winfo_width():
-            self.dx = -1
-        if self.x <= 0:
-            self.dx = +1
-        if self.y >= canvas.winfo_height():
-            self.dy = -1
-        if self.y <= 0:
-            self.dy = +1
-    root.after(10, move)
-		
 
-root = tk.Tk()
-root.geometry("600x400")
+def move(self, canvas):
+	self.erase(canvas)
+	self.x = self.x + self.dx
+	self.y = self.y + self.dy
 
-canvas = tk.Canvas(root, width=600, height=400, bg="white")
-canvas.place(x=0, y=0)
-
-root.after(10, move)
-root.mainloop()
-
-def test(self)
-	print(self.x)
-	print(self.y)
+	self.draw(canvas)
+	if (self.x >= canvas.winfo_width()):
+		self.dx = -1
+	if (self.x <= 0):
+		self.dx = 1
+	if (self.y >= canvas.winfo_height()):
+		self.dy = -1
+	if (self.y <= 0):
+		self.dy = 1
+def erase(self,canvas):
+	canvas.create_oval(self.x - 21, self.y - 21, self.x +
+					21, self.y + 21, fill="white", width=0)
+def draw(self,canvas):
+	canvas.create_oval(self.x - 20, self.y - 20, self.x + 20,
+					self.y + 20, fill=self.color, width=0)
 
 
 balls = [
-    {"x": 400, "y": 300, "dx": +1, "dy": +1, "color": "blue"},
-    {"x": 200, "y": 200, "dx": -1, "dy": +1, "color": "red"},
-    {"x": 300, "y": 200, "dx": +1, "dy": -1, "color": "green"}
-]
+	Ball (400, 300, 1, 1, "red"),
+	Ball (200, 100, -1, 1, "green"),
+	Ball (100, 200, 1, -1, "blue")
+	]
+
+def loop():
+	for b in balls:
+		b.move(canvas)
 
 
+		root.after(10, loop)
+
+root = tk.Tk()
+root.geometry("800x600")
+
+canvas = tk.Canvas(root, width=800, height=600, bg="white")
+canvas.place(x=0, y=0)
+
+root.after(10, loop)
+root.mainloop()
+
+def test(self):
+	print(self.x)
+	print(self.y)
